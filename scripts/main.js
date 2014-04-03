4701bcd07a123be673411c7149a72a79f56ab1d5
kcjpop <kcj.pop.com@gmail.com>
2014-04-04 01:05:36 +0300

    Add other subpages in homepage.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(function() {
-    $('section.landing-image').css('height', $(window).height() - $('nav').height());
+    $('section.landing').css('height', $(window).height() - $('nav').height());
+    $('section.subpage').css('height', $(window).height());
 
     $('.shape').each(function(idx, elm) {
         var _elm = $(elm);
